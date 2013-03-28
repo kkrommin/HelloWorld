fc28049038a3a7befc49b817a708cd07c1cfedd8
Kurt Kromminga <kkrommin@telligen.org>
2013-03-28 15:22:23 -0500

    branch 3 comment

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// change 2 for branch1_fetch_head
+		// another change for branch3_fetch_head
 	}
 
 }
